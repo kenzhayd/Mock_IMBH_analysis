15e90f35a1034db50198113c2c6731bd7e3e2c67
vincent-hb <64926106+vincent-hb@users.noreply.github.com>
2026-03-25 22:56:25 -0300

    Refactor job script: update job parameters and output logging

--- example_scripts/run_octo_192_all_MCMC_centre.sh
+++ example_scripts/run_octo_192_all_MCMC_centre.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #SBATCH --account=def-vhenault
+#SBATCH --job-name=octo_imbh_192
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=192
 #SBATCH --mem-per-cpu=3G
+#SBATCH --time=72:00:00
+#SBATCH --output=logs/octo_imbh_%j.out
+#SBATCH --error=logs/octo_imbh_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=vincent.henault@smu.ca
-#SBATCH --time=72:00:00
+
+mkdir -p logs
 
 module load julia/1.10.10
 
-julia -t 192 ./octo_orbit_julia_192c_18r_all_MCMC_centre.jl  2>&1 | tee output_192_all_18r_MCMC_centre.log
+julia --project=../../Octofitter_imbh.jl -t 192 ./octo_orbit_direct_likelihoods.jl 2>&1 | tee logs/output_direct_likelihoods_192.log
